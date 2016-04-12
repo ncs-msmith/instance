b43d2fb60f02d58ffbc4be722142c3a6e88bfedf
Matt Smith <matt.smith@newcontext.com>
2016-04-12 12:17:01 -0700

    add vpc_id

--- instance.tf
+++ instance.tf
@@ -1,9 +1,11 @@
 # instance/instance.tf
 variable "ip" { type = "string" }
+variable "vpc_id" { type = "string" }
 
 resource "aws_instance" "foo" {
   ami           = "ami-50946030"
   instance_type = "t2.nano"
+  vpc_id        = "${var.vpc_id}"
   provisioner "local-exec" {
     command = "echo ${self.id} - ${var.ip}"
   }
