923968ceb32da829e1b3bd522bd774b32f9f3d09
Matt Smith <matt.smith@newcontext.com>
2016-04-12 11:35:10 -0700

    update

---
+++ instance.tf
@@ -0,0 +1,14 @@
+# instance/instance.tf
+variable "ip" { type = "string" }
+
+resource "aws_instance" "foo" {
+  ami           = "ami-50946030"
+  instance_type = "t2.nano"
+  provisioner "local-exec" {
+    command = "echo ${self.id} - ${var.ip}"
+  }
+}
+
+output "ip" {
+  value = "${aws_instance.*.private_ip}"
+}
